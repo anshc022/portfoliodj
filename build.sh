c635143b8c3bb089024ca03deb5a0da10d3c11a9
anshc022 <rishukrchaurasia007@gmail.com>
2024-10-16 04:04:41 +0530

    fr

--- build.sh
+++ build.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status.
-set -e
+# Install PostgreSQL development package
+apt-get update && apt-get install -y postgresql-server-dev-all
 
-# Ensure pip is installed
-echo "Ensuring pip is installed..."
-python3.9 -m ensurepip --upgrade
+# Install Python dependencies
+pip install -r requirements.txt
 
-# Upgrade pip
-echo "Upgrading pip..."
-python3.9 -m pip install --upgrade pip
-
-# Install dependencies
-echo "Installing dependencies..."
-python3.9 -m pip install -r requirements.txt
-
-# Apply database migrations
-echo "Making Migrations..."
-python3.9 manage.py makemigrations --noinput
-
-echo "Applying Migrations..."
-python3.9 manage.py migrate --noinput
-
-# Collect static files
-echo "Collecting Static Files..."
-python3.9 manage.py collectstatic --noinput --clear
+# Run your build commands
+# Example: python manage.py collectstatic --noinput
